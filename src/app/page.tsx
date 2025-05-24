7734eac4f8af824dde9d29f98bde692818fee400
Vũ Đức Minh <vuducminh27205@gmail.com>
2025-05-24 13:31:49 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import { GoHome } from "react-icons/go";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { FaRegBell } from "react-icons/fa";
 import { CiMail, CiBookmark, CiUser } from "react-icons/ci"; //
+import { BsTwitter } from "react-icons/bs";
 const NAVIGATION_ITEMS = [
-  { title: 'HOME', icon: GoHome },
+  { title: 'Home', icon: GoHome },
   { title: 'Explore', icon: FaMagnifyingGlass },
   { title: 'Notification', icon: FaRegBell },
   { title: 'Messages', icon: CiMail },
@@ -18,12 +19,15 @@ const Page = () => {
     <div className="w-full h-full flex justify-center items-center relative">
       <div className="max-w-screen-lg w-full h-full flex relative">
         {/* Left sidebar NAVIGATION */}
-        <section className="fixed w-72 flex flex-col space-y-2 p-4">
+        <section className="fixed w-72 flex flex-col h-screen space-y-2 my-4">
+        <Link href="/" className="py-2 px-6 text-2xl">
+          <BsTwitter />
+        </Link>
           {NAVIGATION_ITEMS.map((item) => (
             <Link
               href={`/${item.title.toLowerCase()}`}
               key={item.title}
-              className="bg-white/50 flex items-center space-x-2 rounded-3xl p-4 hover:bg-white/70 transition"
+              className="hover:bg-white/10 duration-200 text-xl flex items-center justify-start w-fit space-x-2 rounded-3xl p-4 hover:bg-white/70 transition py-2 px-6"
             >
               <item.icon className="text-xl" />
               <span>{item.title}</span>
